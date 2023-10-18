f23a3982f21e77528d3d4c0ca4504a08c36d4ee9
Douglas Lima <douglas07a@gmail.com>
2023-10-18 13:46:49 -0300

    Inicio do projeto

---
+++ src/main/java/todoApp/Main.java
@@ -0,0 +1,15 @@
+package todoApp;
+
+/**
+ * Douglas Lima
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
